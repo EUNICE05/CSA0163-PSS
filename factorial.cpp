97f632bde3034fc9c2e4525ca735831c874c41db
EUNICE05 <114207104+EUNICE05@users.noreply.github.com>
2023-06-19 09:35:57 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial(int n);
+int main() {
+	int n = 8;
+	printf("factorial of%d: %d", n, factorial(n));
+	
+}
+int factorial(int n){
+	if (n==0||n==1){
+		return 1;
+	}
+	else{
+		return n* factorial(n-1);
+	}
+}
